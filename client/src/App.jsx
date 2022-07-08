c0ea1768d5be991c9df0036df20d32b1da7b181f
mkiss03 <mkiss0516@gmail.com>
2022-07-08 16:47:35 +0200

    elso projekt

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import { Navbar, Welcome } from "./components";
+
+const App = () => (
+  <div className="min-h-screen gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
+  </div>
+);
+
+export default App;
